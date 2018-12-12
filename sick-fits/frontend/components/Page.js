b060b5d133315e62c3bcde725592f4a40cde5d0f
Brock Boren <brock.boren@gmail.com>
2018-12-12 14:32:56 -0600

    Complete intro and setup.

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+import Meta from '../components/Meta';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 100px;
+`
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        <MyButton>Click Me!</MyButton>
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+export default Page;
