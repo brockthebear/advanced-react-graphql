a9c757f0afb6f6c572be67036e88076d62462927
Brock Boren <brock.boren@gmail.com>
2018-12-13 12:39:39 -0600

    Setup server side graphql

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: use express middleware to handle cookies (JWT)
+// TODO: use express middleware to populate current user
+
+server.start({
+  cors: { // only allow endpoint to be visited by approved urls.
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, deets => {
+  console.log(`Server is now running on port http://localhost:${deets.port}`);
+});
