132d01c11885bc54428ff6a351abb2856d003a08
Brock Boren <brock.boren@gmail.com>
2019-01-11 09:29:54 -0600

    Display currently logged in user.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookierParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -6,11 +7,20 @@ const db = require('./db');
 
 const server = createServer();
 
-// TODO: use express middleware to handle cookies (JWT)
 // a middleware is a function that runs in the middle of the request and the response.
 // allow us to access all of the cookies as a formatted object (instead of just a cookie string that it comes in as in the header)
 server.express.use(cookierParser());
-// TODO: use express middleware to populate current user
+// decode the JWT so we can get the user ID on each request.
+server.express.use((req, res, next) => {
+  // pull token out of the request
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the request for future requests to access.
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: { // only allow endpoint to be visited by approved urls.
