63f7d1bd497bd517a823a3e9a26ee93257f8ae09
Brock Boren <brock.boren@gmail.com>
2019-01-11 17:05:42 -0600

    Pass local state of cart to Cart component.

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import { Mutation, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import SickButton from './styles/SickButton';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 
+// `@client` lets apollo know that the data is client side and it can be retrieved directly from the local store.
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
 const Cart = () => {
   return (
-    <CartStyles>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have __ items in your cart.</p>
-      </header>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({ data }) => (
+            <CartStyles open={data.cartOpen}>
+              <header>
+                <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+                <Supreme>Your Cart</Supreme>
+                <p>You have __ items in your cart.</p>
+              </header>
 
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
+              <footer>
+                <p>$10.10</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </CartStyles>
+          )}
+        </Query>
+      )}
+    </Mutation>
   );
 }
 
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
 export default Cart;
