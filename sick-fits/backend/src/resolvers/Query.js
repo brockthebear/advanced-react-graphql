5cf384affd7641ee9e1ae367847a15d7c3ec6142
Brock Boren <brock.boren@gmail.com>
2019-01-17 16:57:47 -0600

    Create OrderList component to display all of a user's orders.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -41,6 +41,17 @@ const Query = {
     }
     // 4. Return the order.
     return order;
+  },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in.');
+    }
+    return ctx.db.query.orders({
+      where: {
+        user: { id: userId },
+      },
+    }, info);
   }
 };
 
