b56c2fcc1ea497201ca47e2089c8c9e6abb3ce80
Brock Boren <brock.boren@gmail.com>
2019-01-17 11:43:57 -0600

    Add ability to remove item from cart.
    Creates the removeFromCart mutation in the backend.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -240,7 +240,30 @@ const Mutations = {
         },
       }
     }, info);
-  }
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the cart item.
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        },
+      },
+      `{ id, user { id }}`
+    );
+    // 1.5 Make sure we found an item.
+    if (!cartItem) throw new Error("No CartItem found!");
+    // 2. Check that the user deleting the cart item is the owner of the cart.
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("You don't have permission to remove that item.");
+    }
+    // 3. Delete that cart item.
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id },
+      }, info
+    );
+  },
 };
 
 module.exports = Mutations;
